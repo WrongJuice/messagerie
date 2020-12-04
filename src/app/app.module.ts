bc05b20f7cb8374df6d31a9c682e18ed7cb28362
Alfred GAILLARD <alfred.gaillard.pro@gmail.com>
2020-12-04 12:27:23 +0100

    Integration of the FIREBASE database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,35 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBS73lRmf_Roj16XP7ySPwpRPQf0X6OWLQ',
+  authDomain: 'messagerie-33100.firebaseapp.com',
+  projectId: 'messagerie-33100',
+  storageBucket: 'messagerie-33100.appspot.com',
+  messagingSenderId: '487159826167',
+  appId: '1:487159826167:web:92b8e00d1e50c3052235f2'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
