7afff7f3b6598c11a51189b00b875ebf54d96f27
Alfred GAILLARD <alfred.gaillard.pro@gmail.com>
2020-12-06 20:19:16 +0100

    Fix import errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,20 +23,6 @@ export const firebaseConfig = {
   appId: '1:487159826167:web:92b8e00d1e50c3052235f2'
 };
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBS73lRmf_Roj16XP7ySPwpRPQf0X6OWLQ',
-  authDomain: 'messagerie-33100.firebaseapp.com',
-  projectId: 'messagerie-33100',
-  storageBucket: 'messagerie-33100.appspot.com',
-  messagingSenderId: '487159826167',
-  appId: '1:487159826167:web:92b8e00d1e50c3052235f2'
-};
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
